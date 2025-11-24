a997b153ceb510ccd96528b5b27f45502ab43e9b
Veikko M. <veikkosakari01@gmail.com>
2025-11-24 21:07:57 +0200

    added backend initialization files with oauth test

---
+++ backend/budget-api/src/main/java/com/budgetapp/backend/Controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.budgetapp.backend.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Welcome!";
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+}
